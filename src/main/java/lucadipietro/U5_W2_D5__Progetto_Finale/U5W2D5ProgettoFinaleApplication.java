d7aaeedbb5c8ecee671928f9419282d0337f8af3
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-06-28 10:14:52 +0200

    Create project and set up dependency, application properties

---
+++ src/main/java/lucadipietro/U5_W2_D5__Progetto_Finale/U5W2D5ProgettoFinaleApplication.java
@@ -0,0 +1,13 @@
+package lucadipietro.U5_W2_D5__Progetto_Finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5ProgettoFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D5ProgettoFinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucadipietro/U5_W2_D5__Progetto_Finale/U5W2D5ProgettoFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package lucadipietro.U5_W2_D5__Progetto_Finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5ProgettoFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
